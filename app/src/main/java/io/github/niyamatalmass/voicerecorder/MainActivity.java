4606eb955f827ddd868228a55d9f4fdd49a29b7d
niyamat <almass.niyamat@gmail.com>
2017-03-09 12:51:13 +0600

    added async task

--- app/src/main/java/io/github/niyamatalmass/voicerecorder/MainActivity.java
+++ app/src/main/java/io/github/niyamatalmass/voicerecorder/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
@@ -12,6 +14,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +24,24 @@ public class MainActivity extends AppCompatActivity {
     private Intent recordIntent;
     private String enteredPhoneNumber;
     private boolean isSpeakerOn;
-    private Button callButton;
+    private Button callButton, playButton, uploadButton;
     private EditText phoneNumberEditText;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phoneNumberEditText = (EditText) findViewById(R.id.phoneEditTextView);
+        callButton = (Button) findViewById(R.id.callButton);
+        playButton = (Button) findViewById(R.id.playButton);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setVisibility(View.GONE);
+
+        playButton.setActivated(false);
+        uploadButton.setActivated(false);
 
         if (getIntent().getAction() != null) {
             String intent = getIntent().getAction();
@@ -35,12 +51,12 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     stopService(recordIntent);
                 }
+                playButton.setActivated(true);
+                uploadButton.setActivated(true);
             }
         }
 
 
-        phoneNumberEditText = (EditText) findViewById(R.id.phoneEditTextView);
-        callButton = (Button) findViewById(R.id.callButton);
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +66,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        if (playButton.isActivated()) {
+            playButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String recordedAudioUrl;
+                    try {
+                        if (enteredPhoneNumber != null) {
+                            recordedAudioUrl = FileHelper.getFilename(enteredPhoneNumber);
+                            seekBar.setVisibility(View.VISIBLE);
+                            PlayRecordedAudio playRecordedAudio = new PlayRecordedAudio();
+                            playRecordedAudio.setSeekBar(seekBar);
+                            playRecordedAudio.execute(recordedAudioUrl);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+        }
+
 
     }
 
@@ -97,6 +136,67 @@ public class MainActivity extends AppCompatActivity {
         audioManager.setMode(AudioManager.MODE_IN_CALL);
         audioManager.setSpeakerphoneOn(b);
         isSpeakerOn = audioManager.isSpeakerphoneOn();
+    }
+
+    public class PlayRecordedAudio extends AsyncTask<String, Integer, Void> implements SeekBar.OnSeekBarChangeListener{
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        SeekBar seekBar;
+
+        public void setSeekBar(SeekBar seekBar) {
+            this.seekBar = seekBar;
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                mediaPlayer.setDataSource(params[0]);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        seekBar.setMax(mediaPlayer.getDuration());
+                        seekBar.setOnSeekBarChangeListener(PlayRecordedAudio.this);
+                    }
+                });
+
+                while (mediaPlayer.isPlaying()) {
+                    publishProgress(mediaPlayer.getCurrentPosition());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            seekBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            mediaPlayer.seekTo(progress / 1000);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
     }
 
 }
